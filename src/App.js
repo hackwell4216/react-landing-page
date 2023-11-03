80aefd55e03b0820b221c633fa18508b737fa7de
hackwell <hackwell.4216@gmail.com>
2023-11-03 19:16:53 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main>
+        <img src="./logo.svg" className="AppIcon icon1" alt="react-logo" />
+        <img src="./logo.svg" className="AppIcon icon2" alt="react-logo" />
+        <img src="./logo.svg" className="AppIcon icon3" alt="react-logo" />
+
+        <img src="./logo.svg" className="AppIcon icon4" alt="react-logo" />
+        <img src="./logo.svg" className="AppIcon icon5" alt="react-logo" />
+        <img src="./logo.svg" className="AppIcon icon6" alt="react-logo" />
+        <h1>Say hello to ReactJS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You will learn a Frontend framework from scratch, to become a Ninja
+          Developer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="btn-awesome">
+          <p>Awesome!</p>
+        </div>
+      </main>
+      <footer>
+        <div className="row">
+          <div className="col-1">
+            <figure>
+              <img src="./images/img_declarative.png" alt="" />
+              <figcaption>
+                <h3>Declarative</h3>
+                <p>React makes it painless to create interactive UIs.</p>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="col-2">
+            <figure>
+              <img src="./images/img_components.png" alt="" />
+              <figcaption>
+                <h3>Components</h3>
+                <p>Build encapsulated components that manage their state.</p>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="col-3">
+            <figure>
+              <img src="./images/img_single_way.png" alt="" />
+              <figcaption>
+                <h3>Single-Way</h3>
+                <p>A set of immutable values are passed to the component's.</p>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="col-4">
+            <figure>
+              <img src="./images/img_jsx.png" alt="" />
+              <figcaption>
+                <h3>JSX</h3>
+                <p>Statically-typed, designed to run on modern browsers.</p>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
